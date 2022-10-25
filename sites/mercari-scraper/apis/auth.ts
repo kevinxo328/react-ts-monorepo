e8078b8d0d71bdcb8d77550d194943857dfb4aa0
Kevin Liu <kevinxo328@gmail.com>
2022-10-25 19:43:54 +0800

    Fix: expose environment variable to browser

--- sites/mercari-scraper/apis/auth.ts
+++ sites/mercari-scraper/apis/auth.ts
@@ -3,7 +3,7 @@ import * as process from "process";
 
 class AuthApi {
   private static getUrl(endpoint: string) {
-    const baseUrl = process.env.API_URL || "http://localhost:8080";
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
     return `${baseUrl}/api${endpoint}`;
   }
   static login<T>(data: T) {
